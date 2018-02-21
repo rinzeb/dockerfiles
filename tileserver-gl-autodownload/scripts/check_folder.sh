055c98355404ed14d8c1bc0b191bbba7195dd014
rinze <rinze.bruining@tno.nl>
2018-02-21 12:04:57 +0100

    Add slightly updated tileserver which downloads mbtiles if not present

---
+++ tileserver-gl-autodownload/scripts/check_folder.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+QUIET=0
+
+echoerr() {
+  if [ "$QUIET" -ne 1 ]; then printf "%s\n" "$*" 1>&2; fi
+}
+
+usage() {
+  exitcode="$1"
+  cat << USAGE >&2
+Usage:
+  $cmdname -d directory [-u url] [-- command args]
+  -d directory                        MBtiles directory
+  -q | --quiet                        Do not output any status messages
+  -u url | --url=url                  URL to download mbtiles file if mbtiles folder is empty
+  -- COMMAND ARGS                     Execute command with args after the test finishes
+USAGE
+  exit "$exitcode"
+}
+
+check_folder() {
+    if [ ! -d "$DIRECTORY" ]; then
+        mkdir -p "$DIRECTORY"
+    fi
+    if [ "$(ls -A $DIRECTORY)" ]; then
+        echo "Directory already contains files, starting tileserver now."
+        exit 0
+    else
+        echo "Empty directory, start downloading $URL."
+        wget -O ./mbtiles "$URL"
+        exit 0
+    fi
+}
+
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    -d)
+    DIRECTORY="$2"
+    shift 2
+    ;;
+    -q | --quiet)
+    QUIET=1
+    shift 1
+    ;;
+    -u)
+    URL="$2"
+    if [ "$URL" = "" ]; then break; fi
+    shift 2
+    ;;
+    --)
+    shift
+    break
+    ;;
+    --help)
+    usage 0
+    ;;
+    *)
+    echoerr "Unknown argument: $1"
+    usage 1
+    ;;
+  esac
+done
+
+if [ "$DIRECTORY" = "" ]; then
+  echoerr "Error: you need to provide a directory to test."
+  usage 2
+fi
+
+check_folder "$@"
